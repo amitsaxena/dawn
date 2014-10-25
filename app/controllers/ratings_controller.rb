32c9d5192d92b7d13949b515e87ab8b928615db6
amitsaxena <amit4uall@gmail.com>
2014-10-26 04:56:11 +0530

    Added ratings views and controllers.

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,9 +1,21 @@
 class RatingsController < ApplicationController
 
-before_filter :authenticate_user!
+  before_filter :authenticate_user!
 
-def index
-  @rating = current_user.rating
-end
+  def index
+    @rating = current_user.rating
+  end
+
+  def create
+    render :json => {:error => "Invalid request!"}.to_json and return if params[:rating].blank?
+    @rating = current_user.rating
+    if @rating
+      @rating.value = params[:rating].to_i
+      @rating.save!
+    else
+      Rating.create!(:value => params[:rating].to_i, :user_id => current_user.id)
+    end
+    render :json => {:success => true}.to_json and return
+  end
 
 end
