360d3f04305fd8fa8e03bfa2322d145e4bb13bc7
amitsaxena <amit4uall@gmail.com>
2014-10-26 06:05:40 +0530

    Allow 0 rating.

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,4 +1,4 @@
 class Rating < ActiveRecord::Base
   belongs_to :user
-  validates :value, :presence => true, :numericality => { :greater_than => 0, :less_than_or_equal_to => 10 }
+  validates :value, :presence => true, :numericality => { :greater_than_or_equal_to => 0, :less_than_or_equal_to => 10 }
 end
